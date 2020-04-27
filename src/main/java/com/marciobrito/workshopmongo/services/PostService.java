daf2e71b535d04e1dbbd627a3af06078db75e329
Marcio Brito <marcio.brito@outlook.com>
2020-04-27 13:11:15 -0300

    Consulta simples com query methods

--- src/main/java/com/marciobrito/workshopmongo/services/PostService.java
+++ src/main/java/com/marciobrito/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.marciobrito.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marciobrito.workshopmongo.entities.Post;
-import com.marciobrito.workshopmongo.entities.User;
 import com.marciobrito.workshopmongo.repository.PostRepository;
 import com.marciobrito.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,5 +21,9 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
