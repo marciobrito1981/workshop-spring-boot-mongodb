244f42bc5e676366af1828b5e077809e17b6a50f
Marcio Brito <marcio.brito@outlook.com>
2020-04-27 15:34:56 -0300

    Consulta com vários critérios

--- src/main/java/com/marciobrito/workshopmongo/resources/util/URL.java
+++ src/main/java/com/marciobrito/workshopmongo/resources/util/URL.java
@@ -2,8 +2,13 @@ package com.marciobrito.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
+	
 	
 	public static String decodeParam(String text) {
 		try {
@@ -11,6 +16,17 @@ public class URL {
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
+		
 	}
 
 }
